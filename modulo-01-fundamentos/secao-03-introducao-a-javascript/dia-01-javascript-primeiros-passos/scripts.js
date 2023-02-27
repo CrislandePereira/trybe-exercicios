16e032b8c70aa91727e9d8913c8df863302c3124
Crislande Pereira <cris_mg1994@hotmail.com>
2023-02-27 11:10:36 -0300

    Primeiro exercicio de variaveis

---
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Crislande";
+const birthCity = "Conceição da Aparecida - MG";
+const birthYear = 1994;
+
+console.log(
+  "Meu nome é " +
+    myName +
+    ", eu nasci em " +
+    birthYear +
+    " na cidade " +
+    birthCity +
+    " ."
+);
