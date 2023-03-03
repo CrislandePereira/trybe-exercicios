5c817143f0482d9ba1522e0d550d7e2cbc863d7d
Crislande Pereira <cris_mg1994@hotmail.com>
2023-03-03 18:21:39 -0300

    exercicio 7

--- modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-le-const-arro-functions-templates-literais/exercicios-2.js
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-le-const-arro-functions-templates-literais/exercicios-2.js
@@ -41,3 +41,13 @@ let nome = "Ivan";
 let sobrenome = "Pires";
 
 console.log(nomeCompleto(nome, sobrenome));
+
+// Exercicio 7
+let speed = 90;
+
+const speedCar = (speed) =>
+  speed >= 120
+    ? `Você ultrapassou o limite de velocidade`
+    : `Você está na velocidade permitida`;
+
+console.log(speedCar(speed));
