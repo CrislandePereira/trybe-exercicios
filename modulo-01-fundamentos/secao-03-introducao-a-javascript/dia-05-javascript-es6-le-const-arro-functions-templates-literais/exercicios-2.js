848bb24a69dcff1d6ffda8b4630d942745f73c2f
Crislande Pereira <cris_mg1994@hotmail.com>
2023-03-03 18:19:29 -0300

    Exercicio 4

--- modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-le-const-arro-functions-templates-literais/exercicios-2.js
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-le-const-arro-functions-templates-literais/exercicios-2.js
@@ -14,3 +14,17 @@ console.log("Idade:", pessoa.idade);
 let favoriteFood = "Lasanha";
 favoriteFood = "Hambúrguer";
 console.log(favoriteFood);
+
+// exercicio 4:
+
+const name = "Adriana";
+const lastName = "Soares";
+console.log(`Olá ${name} ${lastName}!`);
+
+function soma(a, b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log("O resultado da soma de " + a + " + " + b + " é: " + soma(a, b));
