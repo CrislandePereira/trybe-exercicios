5ff49704f4ba45a90aa93e9dbf53d3d0c270230e
Crislande Pereira <cris_mg1994@hotmail.com>
2023-04-24 11:35:11 -0300

    Testes assincronos

--- modulo-02-front-end/secao-01-introducao-frontend-js-assincrono/dia-04-async-await-testes-assincronos/src/main.js
+++ modulo-02-front-end/secao-01-introducao-frontend-js-assincrono/dia-04-async-await-testes-assincronos/src/main.js
@@ -1,29 +1,13 @@
-import Swal from 'sweetalert2';
+import getAddressFromCep from './getAddressFromCep';
+document.querySelector('button').addEventListener('click', handleClick);
 
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const preEl = document.querySelector('pre');
-
-buttonEl.addEventListener('click', handleClick);
-
-async function handleClick() {
-  const cep = inputEl.value;
+export async function handleClick() {
+  const cep = document.querySelector('input').value;
 
   try {
-    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await result.json();
-
-    preEl.innerHTML = JSON.stringify(data);
-    return data;
+    const addressData = await getAddressFromCep(cep);
+    document.querySelector('pre').innerHTML = JSON.stringify(addressData);
   } catch (error) {
-    Swal.fire({
-      title: 'CEP Inválido!',
-      showClass: {
-        popup: 'animate__animated animate__fadeInDown'
-      },
-      hideClass: {
-        popup: 'animate__animated animate__fadeOutUp'
-      }
-    });
+    alert(error.message);
   }
 }
